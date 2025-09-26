d5a3d0f2ba761129951a24436f541c1f83c7683f
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-09-26 09:22:19 +0530

    migrate database configuration from MySQL to PostgreSQL

--- loans/src/main/resources/schema.sql
+++ loans/src/main/resources/schema.sql
@@ -1,14 +1,13 @@
-CREATE TABLE IF NOT EXISTS `loans` (
-                                       `loan_id` int NOT NULL AUTO_INCREMENT,
-                                       `mobile_number` varchar(15) NOT NULL,
-    `loan_number` varchar(100) NOT NULL,
-    `loan_type` varchar(100) NOT NULL,
-    `total_loan` int NOT NULL,
-    `amount_paid` int NOT NULL,
-    `outstanding_amount` int NOT NULL,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`loan_id`)
-    );
+CREATE TABLE IF NOT EXISTS loans (
+                                     loan_id SERIAL PRIMARY KEY,
+                                     mobile_number VARCHAR(15) NOT NULL,
+                                     loan_number VARCHAR(100) NOT NULL,
+                                     loan_type VARCHAR(100) NOT NULL,
+                                     total_loan INT NOT NULL,
+                                     amount_paid INT NOT NULL,
+                                     outstanding_amount INT NOT NULL,
+                                     created_at DATE NOT NULL,
+                                     created_by VARCHAR(20) NOT NULL,
+                                     updated_at DATE DEFAULT NULL,
+                                     updated_by VARCHAR(20) DEFAULT NULL
+);
